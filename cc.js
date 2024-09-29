392bbf9ef591983d90a6aefcab6e3ef2c8dad57c
Shiuan <berserker850330@gmail.com>
2024-09-29 21:36:42 +0800

    first commit

---
+++ cc.js
@@ -0,0 +1,32 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function evil(fn) {
+  return new Function("return " + fn)();
+}
+
+function calculator() {
+  try {
+    display.value = evil(display.value);
+  } catch (err) {
+    display.value = "Error";
+  }
+}
+
+function square() {
+  calculator();
+  const number = display.value;
+  display.value = number * number;
+}
+
+function sqrt() {
+  calculator();
+  const number = display.value;
+  display.value = Math.sqrt(number);
+}
+function clearDisplay() {
+  display.value = "";
+}
